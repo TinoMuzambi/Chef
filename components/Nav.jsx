9a67e61ea876bf219d335c7fb2c8c116d9f0afa6
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 18:06:43 +0200

    Adds scroll for home.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,11 @@ const Nav = () => {
 		<nav>
 			<h3 className="logo">YourChef</h3>
 			<ul className="links">
-				<li>Home</li>
+				<li
+					onClick={() => document.body.scrollIntoView({ behavior: "smooth" })}
+				>
+					Home
+				</li>
 				<li>Works</li>
 				<li>Booking</li>
 			</ul>
