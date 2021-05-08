ea4b28e74c2ac16b657ee9445fbf56be9dbca365
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 16:23:40 +0200

    Adds initial styling for hero.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,11 @@
 const Hero = () => {
 	return (
 		<section className="hero">
-			<div className="content">
+			<div className="text">
 				<h1 className="title">YourChef</h1>
 				<p className="subtitle">Truly Gourmet</p>
 			</div>
+			<div className="bg-image"></div>
 		</section>
 	);
 };
