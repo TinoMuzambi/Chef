76ceb72b3368cffd4b78abad02a63c9c9d8099c2
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 17:27:15 +0200

    Removes extra work.

--- data/works.js
+++ data/works.js
@@ -13,13 +13,6 @@ const works = [
 			"https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
 		serving: "Serves 4 - 6",
 	},
-	{
-		id: 2,
-		title: "Summer Salad",
-		image:
-			"https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
-		serving: "Serves 4 - 6",
-	},
 ];
 
 export default works;
