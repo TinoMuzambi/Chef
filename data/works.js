906ef857d733b8b50e8c093b69d103a9363646aa
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 16:52:29 +0200

    Adds data file.

---
+++ data/works.js
@@ -0,0 +1,18 @@
+const works = [
+	{
+		id: 0,
+		title: "Roasted Eggplants",
+		image:
+			"https://images.unsplash.com/photo-1522579431750-7de6093e14d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+		serving: "Serves 3 - 5",
+	},
+	{
+		id: 1,
+		title: "Summer Salad",
+		image:
+			"https://images.unsplash.com/photo-1572449043416-55f4685c9bb7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+		serving: "Serves 4 - 6",
+	},
+];
+
+export default works;
