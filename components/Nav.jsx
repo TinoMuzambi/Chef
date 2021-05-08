27624078de701a314c5ffac5d04ac156995a0c08
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 16:08:28 +0200

    Adds nav markup.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,14 @@
 const Nav = () => {
-	return <div></div>;
+	return (
+		<nav>
+			<h3 className="logo">YourChef</h3>
+			<ul className="links">
+				<li className="link">Home</li>
+				<li className="link">Works</li>
+				<li className="link">Booking</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Nav;
