75772085a338412e844d7a313a1255b095d3d24b
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 17:57:08 +0200

    Adds footer markup.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-	return <div></div>;
+	return (
+		<footer>
+			<div className="socials"></div>
+			<p className="rights">Copyright © YourChef 2021</p>
+			<a href="https://bit.ly/TinoLinkedIn">Built by Tino Muzambi</a>
+		</footer>
+	);
 };
 
 export default Footer;
