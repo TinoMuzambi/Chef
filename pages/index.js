7a2fbb4f6c73bb0211ddcf29789976247b1f1804
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 15:55:49 +0200

    Adds nav and head.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,69 @@
+import Head from "next/head";
+
 import Nav from "../components/Nav";
+
+const keywords = "";
+const title = "Your Chef";
+const description = "";
+const image = "";
+const url = "";
 
 const Home = () => (
 	<>
+		<Head>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="theme-color" content="#000000" />
+			<meta name="keywords" content={keywords} />
+			<meta name="description" content={description} />
+
+			{/* <!-- Google / Search Engine Tags --> */}
+			<meta itemProp="name" content={title} />
+			<meta itemProp="description" content={description} />
+			<meta itemProp="image" content={image} />
+
+			{/* <!-- Facebook Meta Tags --> */}
+			<meta property="og:url" content={url} />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:image" content={image} />
+
+			{/* <!-- Twitter Meta Tags --> */}
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={description} />
+			<meta name="twitter:image" content={image} />
+
+			<meta charSet="utf-8" />
+			<link rel="icon" href="/favicon.ico" />
+			<meta
+				name="Elle Zeka"
+				content="People buy with their eyes, we worry about what they see."
+			/>
+			<link rel="apple-touch-icon" href="/logo192.png" />
+			<link rel="manifest" href="/manifest.json" />
+			<title>{title}</title>
+
+			{/* AOS Library */}
+			<link
+				rel="stylesheet"
+				href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css"
+				integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w=="
+				crossOrigin="anonymous"
+			/>
+
+			{/* Mapbox Library */}
+			<link
+				href="https://api.mapbox.com/mapbox-gl-js/v1.12.0/mapbox-gl.css"
+				rel="stylesheet"
+			/>
+
+			<link rel="preconnect" href="https://api.storyblok.com" />
+			<link rel="preconnect" href="https://a.storyblok.com" />
+			<link rel="preconnect" href="https://www.google-analytics.com" />
+			<link rel="preconnect" href="https://www.api.mapbox.com" />
+			<link rel="preconnect" href="https://www.mapbox.com" />
+		</Head>
 		<Nav />
 	</>
 );
