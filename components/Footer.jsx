3959e92625e0f4f6a7c4c8e6d9f91a28827874d0
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 17:58:53 +0200

    Adds footer styling.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,9 @@ const Footer = () => {
 		<footer>
 			<div className="socials"></div>
 			<p className="rights">Copyright © YourChef 2021</p>
-			<a href="https://bit.ly/TinoLinkedIn">Built by Tino Muzambi</a>
+			<a href="https://bit.ly/TinoLinkedIn" target="__blank">
+				Built by Tino Muzambi
+			</a>
 		</footer>
 	);
 };
