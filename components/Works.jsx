98200045d5f386de08c7b7ef5f23ab775ed60cfe
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 16:46:40 +0200

    Adds carousel.

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,3 +1,12 @@
+import {
+	CarouselProvider,
+	Slider,
+	Slide,
+	ButtonBack,
+	ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+
 const Works = () => {
 	return (
 		<section className="works">
@@ -5,7 +14,21 @@ const Works = () => {
 				<p className="title">Straight From the Source</p>
 				<h2 className="subtitle">YourChef Works</h2>
 			</div>
-			<div className="carousel"></div>
+			<div className="carousel">
+				<CarouselProvider
+					naturalSlideWidth={100}
+					naturalSlideHeight={40}
+					totalSlides={3}
+				>
+					<Slider>
+						<Slide index={0}>I am the first Slide.</Slide>
+						<Slide index={1}>I am the second Slide.</Slide>
+						<Slide index={2}>I am the third Slide.</Slide>
+					</Slider>
+					<ButtonBack>Back</ButtonBack>
+					<ButtonNext>Next</ButtonNext>
+				</CarouselProvider>
+			</div>
 		</section>
 	);
 };
