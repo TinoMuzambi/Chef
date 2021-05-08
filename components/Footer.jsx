1f715ad632c3024c4dceaff599abc52162290b3b
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 18:02:09 +0200

    Adds social links.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,25 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
 	return (
 		<footer>
-			<div className="socials"></div>
+			<ul className="socials">
+				<li>
+					<a href="#" target="__blank">
+						<FaFacebookF />
+					</a>
+				</li>
+				<li>
+					<a href="#" target="__blank">
+						<FaInstagram />
+					</a>
+				</li>
+				<li>
+					<a href="#" target="__blank">
+						<FaTwitter />
+					</a>
+				</li>
+			</ul>
 			<p className="rights">Copyright © YourChef 2021</p>
 			<a href="https://bit.ly/TinoLinkedIn" target="__blank">
 				Built by Tino Muzambi
