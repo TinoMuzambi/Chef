2433ad5519df6d8b1cc662d5e57d723016b916e0
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-08 16:34:03 +0200

    Adds initial works markup.

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,5 +1,13 @@
 const Works = () => {
-	return <div></div>;
+	return (
+		<section className="works">
+			<div className="lead">
+				<p className="title">Straight From the Source</p>
+				<h2 className="subtitle">YourChef Works</h2>
+			</div>
+			<div className="carousel"></div>
+		</section>
+	);
 };
 
 export default Works;
